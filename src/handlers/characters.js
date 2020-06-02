4c2fec1ac46c731f42deb0bbda3ccc465d71a59c
Joao Lucas <jlcarruda3@gmail.com>
2020-06-02 11:20:51 -0300

    Partial implementation of character listing

--- src/handlers/characters.js
+++ src/handlers/characters.js
@@ -1,11 +1,31 @@
 const { app, serverless } = require('../header')
-const { isAuthenticated }  = require('../middlewares')
-const { FailedAuthError, FailedSignUpError } = require('../errors')
+const { isAuthenticated, ErrorHandler }  = require('../middlewares')
+const { NotAuthorizedError } = require('../errors')
 
-app.get('/user/characters', isAuthenticated, (req, res) => {
+app.get('/users/characters', isAuthenticated, (req, res, next) => {
+  // if (!req.auth || !req.auth.user) return next(new NotAuthorizedError())
 
+  try {
+    const characters = req.auth.user.characters || []
+    res.status(200).json({
+      data: characters.map(c => {
+        const { _id, ...attributes } = c
+        return {
+          type: 'characters',
+          id: c._id,
+          attributes
+        }
+      })
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
-app.get('/user/characters/:id', isAuthenticated, (req, res) => {
-
+app.get('/users/characters/:id', isAuthenticated, (req, res) => {
+  // if (!req.auth || !req.auth.user) return next(new NotAuthorizedError())
 })
+
+app.use(ErrorHandler)
+
+module.exports.handler = serverless(app)
